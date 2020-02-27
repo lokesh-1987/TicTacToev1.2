f8ead2f8e162dd1625a7d4e3f86717b842cf9efa
Lokesh Varshney <var.lokesh@gmail.com>
2020-02-27 22:09:42 +0530

    Extracted constant for '' as EMPTY_STRING

--- src/main/java/com/kata/tictactoe/TicTacToeGame.java
+++ src/main/java/com/kata/tictactoe/TicTacToeGame.java
@@ -1,7 +1,10 @@
 package com.kata.tictactoe;
 
 class TicTacToeGame {
+
+    private static final String EMPTY_STRING = "";
+
     String playGame(int row, int col, char playerSymbol) {
-        return "";
+        return EMPTY_STRING;
     }
 }
