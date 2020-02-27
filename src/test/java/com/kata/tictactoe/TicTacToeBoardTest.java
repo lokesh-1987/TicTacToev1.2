a4b23031e6bb55df2396d2bde9d0edbe0ae8abc3
Lokesh Varshney <var.lokesh@gmail.com>
2020-02-27 15:43:35 +0530

    Added static import for assertThat and is methods

--- src/test/java/com/kata/tictactoe/TicTacToeBoardTest.java
+++ src/test/java/com/kata/tictactoe/TicTacToeBoardTest.java
@@ -1,14 +1,15 @@
 package com.kata.tictactoe;
 
-import org.hamcrest.core.Is;
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class TicTacToeBoardTest {
 
     @Test
     public void ticTacToeBoardShouldReturnRowLengthOf3AfterDeclaration() {
         TicTacToeBoard ticTacToeBoard = new TicTacToeBoard();
-        Assert.assertThat(ticTacToeBoard.board.length, Is.is(3));
+        assertThat(ticTacToeBoard.board.length, is(3));
     }
 }
