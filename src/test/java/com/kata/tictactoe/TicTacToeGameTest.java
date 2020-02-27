2225cac553012683b58579d8c1f9799df3df4b54
Lokesh Varshney <var.lokesh@gmail.com>
2020-02-27 22:11:43 +0530

    Extracted constant for '' as EMPTY_STRING in test class

--- src/test/java/com/kata/tictactoe/TicTacToeGameTest.java
+++ src/test/java/com/kata/tictactoe/TicTacToeGameTest.java
@@ -7,9 +7,11 @@ import static org.junit.Assert.assertThat;
 
 public class TicTacToeGameTest {
 
+    private static final String EMPTY_STRING = "";
+
     @Test
     public void ticTacToeGameShouldReturn_EmptyString_IfPlayer_X_MovesOn_0_0_Position() {
         TicTacToeGame ticTacToeGame = new TicTacToeGame();
-        assertThat(ticTacToeGame.playGame(0,0, 'X'), is(""));
+        assertThat(ticTacToeGame.playGame(0,0, 'X'), is(EMPTY_STRING));
     }
 }
