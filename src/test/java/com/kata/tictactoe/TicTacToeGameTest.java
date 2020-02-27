563cc15325d8ba4c039517b5cfbe119031d78a1b
Lokesh Varshney <var.lokesh@gmail.com>
2020-02-27 22:27:38 +0530

    Added constant for X or 0 as CROSS and ZERO

--- src/test/java/com/kata/tictactoe/TicTacToeGameTest.java
+++ src/test/java/com/kata/tictactoe/TicTacToeGameTest.java
@@ -9,24 +9,26 @@ public class TicTacToeGameTest {
 
     private static final String EMPTY_STRING = "";
     private static final String GAME_IS_DRAW = "Game is Draw!";
+    private static final char CROSS = 'X';
+    private static final char ZERO = '0';
 
     @Test
     public void ticTacToeGameShouldReturn_EmptyString_IfPlayer_X_MovesOn_0_0_Position() {
         TicTacToeGame ticTacToeGame = new TicTacToeGame();
-        assertThat(ticTacToeGame.playGame(0,0, 'X'), is(EMPTY_STRING));
+        assertThat(ticTacToeGame.playGame(0,0, CROSS), is(EMPTY_STRING));
     }
 
     @Test
     public void ticTacToeGameShouldReturn_GameIsDrawMessage_ifThePlayersMoveAlternativelyUntilGameIsDraw() {
         TicTacToeGame ticTacToeGame = new TicTacToeGame();
-        assertThat(ticTacToeGame.playGame(0,0,'X'), is(EMPTY_STRING));
-        assertThat(ticTacToeGame.playGame(0,1,'0'), is(EMPTY_STRING));
-        assertThat(ticTacToeGame.playGame(0,2,'X'), is(EMPTY_STRING));
-        assertThat(ticTacToeGame.playGame(1,0,'0'), is(EMPTY_STRING));
-        assertThat(ticTacToeGame.playGame(1,1,'X'), is(EMPTY_STRING));
-        assertThat(ticTacToeGame.playGame(1,2,'0'), is(EMPTY_STRING));
-        assertThat(ticTacToeGame.playGame(2,1,'X'), is(EMPTY_STRING));
-        assertThat(ticTacToeGame.playGame(2,0,'0'), is(EMPTY_STRING));
-        assertThat(ticTacToeGame.playGame(2,2,'X'), is(GAME_IS_DRAW));
+        assertThat(ticTacToeGame.playGame(0,0, CROSS), is(EMPTY_STRING));
+        assertThat(ticTacToeGame.playGame(0,1, ZERO), is(EMPTY_STRING));
+        assertThat(ticTacToeGame.playGame(0,2, CROSS), is(EMPTY_STRING));
+        assertThat(ticTacToeGame.playGame(1,0, ZERO), is(EMPTY_STRING));
+        assertThat(ticTacToeGame.playGame(1,1, CROSS), is(EMPTY_STRING));
+        assertThat(ticTacToeGame.playGame(1,2, ZERO), is(EMPTY_STRING));
+        assertThat(ticTacToeGame.playGame(2,1, CROSS), is(EMPTY_STRING));
+        assertThat(ticTacToeGame.playGame(2,0, ZERO), is(EMPTY_STRING));
+        assertThat(ticTacToeGame.playGame(2,2, CROSS), is(GAME_IS_DRAW));
     }
 }
