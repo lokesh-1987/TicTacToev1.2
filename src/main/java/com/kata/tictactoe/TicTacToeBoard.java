9efb8bbb310da1a64ab60a40973d8bc76d223a87
Lokesh Varshney <var.lokesh@gmail.com>
2020-02-28 00:36:07 +0530

    refactored the code of returning 00 as int array for position 1

--- src/main/java/com/kata/tictactoe/TicTacToeBoard.java
+++ src/main/java/com/kata/tictactoe/TicTacToeBoard.java
@@ -1,10 +1,17 @@
 package com.kata.tictactoe;
 
+import java.util.Arrays;
+import java.util.List;
+
 class TicTacToeBoard {
     private static final char HYPHEN = '-';
     private static final char CROSS = 'X';
     private static final char ZERO = '0';
     char[][] board;
+    List<int[]> boardIndex = Arrays.asList(new int[][]{
+            {0,0},{0,1},{0,2},
+            {1,0},{1,1},{1,2},
+            {2,0},{2,1},{2,2}});
 
     TicTacToeBoard() {
         this.board = new char[3][3];
@@ -116,6 +123,10 @@ class TicTacToeBoard {
     }
 
     int[] getBoardIndex(int position) {
-        return new int[]{0,0};
+        switch (position) {
+            case 1 :
+                return this.boardIndex.get(0);
+        }
+        return null;
     }
 }
