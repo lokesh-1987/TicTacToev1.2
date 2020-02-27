8a3abf5004dcea2a33c19d6579a7d3cc878af232
Lokesh Varshney <var.lokesh@gmail.com>
2020-02-27 22:24:04 +0530

    Added a test to return Game_is_draw message untill the board is full

--- src/main/java/com/kata/tictactoe/TicTacToeGame.java
+++ src/main/java/com/kata/tictactoe/TicTacToeGame.java
@@ -3,8 +3,13 @@ package com.kata.tictactoe;
 class TicTacToeGame {
 
     private static final String EMPTY_STRING = "";
+    private TicTacToeBoard ticTacToeBoard = new TicTacToeBoard();
 
     String playGame(int row, int col, char playerSymbol) {
+        ticTacToeBoard.setValue(row,col,playerSymbol);
+        if (ticTacToeBoard.isBoardFull()) {
+            return "Game is Draw!";
+        }
         return EMPTY_STRING;
     }
 }
--- src/test/java/com/kata/tictactoe/TicTacToeGameTest.java
+++ src/test/java/com/kata/tictactoe/TicTacToeGameTest.java
@@ -14,4 +14,18 @@ public class TicTacToeGameTest {
         TicTacToeGame ticTacToeGame = new TicTacToeGame();
         assertThat(ticTacToeGame.playGame(0,0, 'X'), is(EMPTY_STRING));
     }
+
+    @Test
+    public void ticTacToeGameShouldReturn_GameIsDrawMessage_ifThePlayersMoveAlternativelyUntilGameIsDraw() {
+        TicTacToeGame ticTacToeGame = new TicTacToeGame();
+        assertThat(ticTacToeGame.playGame(0,0,'X'), is(EMPTY_STRING));
+        assertThat(ticTacToeGame.playGame(0,1,'0'), is(EMPTY_STRING));
+        assertThat(ticTacToeGame.playGame(0,2,'X'), is(EMPTY_STRING));
+        assertThat(ticTacToeGame.playGame(1,0,'0'), is(EMPTY_STRING));
+        assertThat(ticTacToeGame.playGame(1,1,'X'), is(EMPTY_STRING));
+        assertThat(ticTacToeGame.playGame(1,2,'0'), is(EMPTY_STRING));
+        assertThat(ticTacToeGame.playGame(2,1,'X'), is(EMPTY_STRING));
+        assertThat(ticTacToeGame.playGame(2,0,'0'), is(EMPTY_STRING));
+        assertThat(ticTacToeGame.playGame(2,2,'X'), is("Game is Draw!"));
+    }
 }
