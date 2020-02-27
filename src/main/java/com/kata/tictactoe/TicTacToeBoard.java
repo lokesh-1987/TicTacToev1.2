31550fd89cd61301daacef28cbb5147e9d50ec0f
Lokesh Varshney <var.lokesh@gmail.com>
2020-02-27 15:55:31 +0530

    Updated scope for setUpBoard method to package-private

--- src/main/java/com/kata/tictactoe/TicTacToeBoard.java
+++ src/main/java/com/kata/tictactoe/TicTacToeBoard.java
@@ -7,7 +7,7 @@ class TicTacToeBoard {
         this.board = new char[3][3];
     }
 
-    public void setUpBoard() {
+    void setUpBoard() {
         for (int row = 0; row < this.board.length; row++) {
             for (int column = 0; column < this.board[row].length; column++) {
                 this.board[row][column] = '-';
