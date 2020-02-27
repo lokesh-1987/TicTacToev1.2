842adf9459d867ea20fdf9db8fe21fa87a252f22
Lokesh Varshney <var.lokesh@gmail.com>
2020-02-27 15:51:32 +0530

    Moved ticTacToeBoard local variable to a field variable and initialized it in setUp method

--- src/test/java/com/kata/tictactoe/TicTacToeBoardTest.java
+++ src/test/java/com/kata/tictactoe/TicTacToeBoardTest.java
@@ -1,5 +1,6 @@
 package com.kata.tictactoe;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
@@ -7,15 +8,20 @@ import static org.junit.Assert.assertThat;
 
 public class TicTacToeBoardTest {
 
+    private TicTacToeBoard ticTacToeBoard;
+
+    @Before
+    public void setUp() {
+        ticTacToeBoard = new TicTacToeBoard();
+    }
+
     @Test
     public void ticTacToeBoardShouldReturnRowLengthOf3AfterDeclaration() {
-        TicTacToeBoard ticTacToeBoard = new TicTacToeBoard();
         assertThat(ticTacToeBoard.board.length, is(3));
     }
 
     @Test
     public void ticTacToeBoardShouldReturn_HyphenSymbol_AtIndex_00_AfterSetUp() {
-        TicTacToeBoard ticTacToeBoard = new TicTacToeBoard();
         ticTacToeBoard.setUpBoard();
         assertThat(ticTacToeBoard.board[0][0], is('-'));
     }
