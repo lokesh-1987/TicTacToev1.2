d9296361dc730b451e3a751049afdfaa8d8062d5
Lokesh Varshney <var.lokesh@gmail.com>
2020-02-27 22:30:30 +0530

    moved ticTacToeGame local variable as a field variable and initialized it in setUp method

--- src/test/java/com/kata/tictactoe/TicTacToeGameTest.java
+++ src/test/java/com/kata/tictactoe/TicTacToeGameTest.java
@@ -1,5 +1,6 @@
 package com.kata.tictactoe;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
@@ -11,16 +12,20 @@ public class TicTacToeGameTest {
     private static final String GAME_IS_DRAW = "Game is Draw!";
     private static final char CROSS = 'X';
     private static final char ZERO = '0';
+    private TicTacToeGame ticTacToeGame;
+
+    @Before
+    public void setUp() {
+        ticTacToeGame = new TicTacToeGame();
+    }
 
     @Test
     public void ticTacToeGameShouldReturn_EmptyString_IfPlayer_X_MovesOn_0_0_Position() {
-        TicTacToeGame ticTacToeGame = new TicTacToeGame();
         assertThat(ticTacToeGame.playGame(0,0, CROSS), is(EMPTY_STRING));
     }
 
     @Test
     public void ticTacToeGameShouldReturn_GameIsDrawMessage_ifThePlayersMoveAlternativelyUntilGameIsDraw() {
-        TicTacToeGame ticTacToeGame = new TicTacToeGame();
         assertThat(ticTacToeGame.playGame(0,0, CROSS), is(EMPTY_STRING));
         assertThat(ticTacToeGame.playGame(0,1, ZERO), is(EMPTY_STRING));
         assertThat(ticTacToeGame.playGame(0,2, CROSS), is(EMPTY_STRING));
